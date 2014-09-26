d7b3c17c3886ee4d67265a634e107ea85ed10b14
Thomas S. Thelly <thomasthelly@Thomass-MacBook-Pro.local>
2014-09-26 21:53:04 +0530

    Some problems ... Gotta fix

--- src/StudentLoggedInPanel.java
+++ src/StudentLoggedInPanel.java
@@ -9,6 +9,8 @@ public class StudentLoggedInPanel extends JPanel {
 	public StudentLoggedInPanel(int stud_id) {
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.
 		add(new StudentLoggedInSidePanel(stud_id), BorderLayout.WEST);
 		add(new StudentLoggedInMainPanel(stud_id), BorderLayout.CENTER);
 	}
